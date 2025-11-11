e544c7761288bbf8a65a3b667f6ec33c3fe8f9c2
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2025-11-11 17:22:57 +0100

    fix plastic to ph

--- oceanstate_analysis/analysis/preprocessing.py
+++ oceanstate_analysis/analysis/preprocessing.py
@@ -2,7 +2,6 @@ import pandas as pd
 from .config import URLS, REQUEST_OPTIONS, COLUMN_NAMES, RAW_DATA_FILES
 
 def load_and_clean_ph_data()->pd.DataFrame:
-    """Charge et nettoie les données du pH de l'eau de mer."""
     df = pd.read_csv(URLS["seawater_ph"], storage_options=REQUEST_OPTIONS)
 
     column_mapping = COLUMN_NAMES["seawater_ph"]
@@ -12,7 +11,6 @@ def load_and_clean_ph_data()->pd.DataFrame:
 
 
 def load_and_clean_plastic_data() -> pd.DataFrame:
-    """Charge et nettoie les données des microplastiques."""
     df = pd.read_csv(URLS["microplastics"], storage_options=REQUEST_OPTIONS)
 
     # Renommage des colonnes
